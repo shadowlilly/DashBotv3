d98af98d32c1de77562aa0d9af75b109946b8c7f
unknown <programmer@bitsbots.club>
2019-06-25 18:46:26 -0700

    Another test

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 bot.login(process.env.temptoken);
-bot.on("ready, () => {
+bot.on("ready", () => {
 	bot.channels.get("484808742160957442").send("It worked, bitch");
 });
