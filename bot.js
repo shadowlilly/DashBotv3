803ed487f449dec925f9163d10bdddc8c0c244fe
ShadowLilly <44298093+shadowlilly@users.noreply.github.com>
2019-06-27 18:34:40 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,21 @@
 const Discord = require("discord.js");
 const Client = new Discord.Client();
+const XMLHTTPRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var token = process.env.localtoken;
-
+var request = new XMLHTTPRequest();
+request.addEventListener("readystatechange", function() {
+    if(request.readyState == request.DONE) {
+        token += request.responseText;
+        Client.login(token).then(function() {
+            token = “FECK OFF”;
+            request = null;
+        });
+    }
+});
+request.open("GET", "https://dashbotauth.herokuapp.com");
+request.send();
 
 Client.on("ready", function() {
+    console.log("Success");
 });
-
