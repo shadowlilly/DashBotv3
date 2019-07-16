b3e977180f821f44644b53c00025619a09e7dab0
Lilly <shadowlilly013@gmail.com>
2019-07-16 12:24:41 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const Discord = require("discord.js");
 const Client = new Discord.Client();
 const XMLHTTPRequest = require("xmlhttprequest").XMLHttpRequest;
+
+http.createServer(function (request, response) {
+
+   console.log('request starting for ');
+   console.log(request);
+
+   var filePath = '.' + request.url;
+   if (filePath == './')
+       filePath = './index.html';
+
+   console.log(filePath);
+   var extname = path.extname(filePath);
+   var contentType = 'text/html';
+   switch (extname) {
+       case '.js':
+           contentType = 'text/javascript';
+           break;
+       case '.css':
+           contentType = 'text/css';
+           break;
+   }
+
+   path.exists(filePath, function(exists) {
+
+       if (exists) {
+           fs.readFile(filePath, function(error, content) {
+               if (error) {
+                   response.writeHead(500);
+                   response.end();
+               }
+               else {
+                   response.writeHead(200, { 'Content-Type': contentType });
+                   response.end(content, 'utf-8');
+               }
+           });
+       }
+       else {
+           response.writeHead(404);
+           response.end();
+       }
+   });
+
+}).listen(process.env.PORT);
 
 var token = process.env.localtoken;
 var request = new XMLHTTPRequest();
