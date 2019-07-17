d98665c67c8b1f9a0e2242dc1715866f3e9ae214
Lilly <shadowlilly013@gmail.com>
2019-07-16 22:49:30 -0700

    Added more websocket

--- bot.js
+++ bot.js
@@ -1,3 +1,31 @@
 const Discord = require("discord.js");
 const Client = new Discord.Client();
-var token = process.env.localtoken;
+
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+var firsttoken = process.env.localtoken;
+var secondtoken = "PLACEHOLDER"
+(async () => {
+    secondtoken = await getToken();
+})();
+
+async function getToken() {
+  await sleep(1000);
+  client.connect("SELECT TOP 1 temptoken FROM keys", (err, res) => {
+    if(err) throw err;
+    console.log(res.rows[0]);
+  });
+
+  client.query()
+
+}
+
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
