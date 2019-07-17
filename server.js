157c9a38f0007cd5e016b21e8d4ec57beb2d3bb9
Lilly <shadowlilly013@gmail.com>
2019-07-17 01:05:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ webclient.on('connect', function(connection) {
   });
 });
 
-webclient.connect("wss://dashbotauth.herokuapp.com/server.js", 'DBCP-KEY_' + process.env.socketkey);
+webclient.connect("wss://dashbotauth.herokuapp.com/server.js", 'DBCP');
