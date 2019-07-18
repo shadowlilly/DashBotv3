92a97aeb0c74eec4d82f95188fde19e187b84b9a
Lilly <shadowlilly013@gmail.com>
2019-07-18 11:30:24 -0700

    Update parseMessage.js

--- parseMessage.js
+++ parseMessage.js
@@ -1,6 +1,11 @@
 function parseMessage(message) {
-  console.log("Receieved a message: " + message);
-  return 0;
+
+  if(message.channel == "591741482831183892") {
+
+    message.channel.send(message.content);
+
+  }
+
 }
 
 module.exports = parseMessage;
