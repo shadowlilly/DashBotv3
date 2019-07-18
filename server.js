bee46c74e89bbc0a2f8dc581cc44c5b992963989
Lilly <shadowlilly013@gmail.com>
2019-07-17 20:22:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -26,51 +26,51 @@ async function launchServer() {
 
   console.log("Now attempting to establish a WebSocket connection");
 
-  webclient.connect("wss://dashbotauth.herokuapp.com", "dbcp_key-" + process.env.socketkey);
+  socket.connect("wss://dashbotauth.herokuapp.com", "dbcp_key-" + process.env.socketkey);
+
+  socket.on("error", function(err) {
+    console.log("An error occured with the websocket. " + err);
+  })
+
+  socket.on('connectFailed', function(err) {
+      console.log("Connection to the websocket failed. " + err);
+  });
+
+  socket.on('connect', function(connection) {
+
+    console.log("Successfully connected to WebSocket");
+    setInterval(keepAlive, 300000);
+
+    connection.on('error', function(err) {
+        console.log("Connection Error: " + err);
+    });
+
+    connection.on('close', function() {
+        console.log("The server has shut down the connection. Sending shutdown signal to bot");
+        database.query("UPDATE keys SET shutdown = true").then(function(res) {
+          console.log("Bot has been given the shutdown signal. Shutting down web client...")
+          process.exit(0);
+        }).catch(function(err) {
+          console.log("An error occured signaling shutdown to bot. " + err);
+          console.log("Shutting down web client...");
+          process.exit(1);
+        })
+    });
+
+    connection.on('message', function(message) {
+        if(message.utf8Data.startsWith("TOKEN IS ")) {
+          console.log("The token has been receieved. Sending to bot.");
+          database.query("UPDATE keys SET temptoken = " + message.utf8Data.substring(9)).then(function() {
+            console.log("The token has been sent to the bot");
+          }).catch(function(err) {
+            console.log("An error occured when forwarding the token. " + err);
+          });
+        }
+    });
+
+  });
 
 }
-
-webclient.on("error", function(err) {
-  console.log("An error occured with the websocket. " + err);
-})
-
-webclient.on('connectFailed', function(err) {
-    console.log("Connection to the websocket failed. " + err);
-});
-
-webclient.on('connect', function(connection) {
-
-  console.log("Successfully connected to WebSocket");
-  setInterval(keepAlive, 300000);
-
-  connection.on('error', function(err) {
-      console.log("Connection Error: " + err);
-  });
-
-  connection.on('close', function() {
-      console.log("The server has shut down the connection. Sending shutdown signal to bot");
-      database.query("UPDATE keys SET shutdown = true").then(function(res) {
-        console.log("Bot has been given the shutdown signal. Shutting down web client...")
-        process.exit(0);
-      }).catch(function(err) {
-        console.log("An error occured signaling shutdown to bot. " + err);
-        console.log("Shutting down web client...");
-        process.exit(1);
-      })
-  });
-
-  connection.on('message', function(message) {
-      if(message.utf8Data.startsWith("TOKEN IS ")) {
-        console.log("The token has been receieved. Sending to bot.");
-        database.query("UPDATE keys SET temptoken = " + message.utf8Data.substring(9)).then(function() {
-          console.log("The token has been sent to the bot");
-        }).catch(function(err) {
-          console.log("An error occured when forwarding the token. " + err);
-        });
-      }
-  });
-
-});
 
 function keepAlive() {
 
