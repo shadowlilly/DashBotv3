5626021d0bbb92d764a1d0c6231e90b4e9f9112e
Lilly <shadowlilly013@gmail.com>
2019-07-17 20:25:32 -0700

    Update globals.js

--- globals.js
+++ globals.js
@@ -5,3 +5,15 @@ const request = require('request');
 const Discord = require("discord.js");
 const WebSocketClient = require("websocket").client;
 const bot = new Discord.Client();
+
+module.exports = {
+
+  http: http,
+  fs: fs,
+  path: path,
+  request: request,
+  Discord: Discord,
+  WebSocketClient: WebSocketClient,
+  bot: bot
+
+}
